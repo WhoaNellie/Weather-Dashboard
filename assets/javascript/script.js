9e728a4fe022bee3d7bcfbdfd40b2b7d50b9c06a
Nellie <fabybacenelson@gmail.com>
2019-12-13 15:11:50 -0700

    fixed 5day dates and positioning

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -120,7 +120,7 @@ $(document).ready(function(){
                 let fiveIcon = response.list[(i*8)+4].weather[0].icon;
 
                 let day = $("<div>");
-                let date = $("<p>").text(moment().add(i+1, "days").calendar());
+                let date = $("<p>").text(moment().add(i+1, "days").format("(MM/DD/YY)"));
                 let icon = $("<img>").attr({src : "http://openweathermap.org/img/wn/"+ fiveIcon +"@2x.png"})
 
                 let temp = $("<p>").text("Temp: " + totalTemp + " F");
